6a220332464fe325380427c1151b49117586f8f0
gusweda219 <57088126+gusweda219@users.noreply.github.com>
2024-05-13 15:06:14 +0800

    change select filter to dropdown

--- src/views/pengawasan/PencabutanReklame.js
+++ src/views/pengawasan/PencabutanReklame.js
@@ -33,7 +33,6 @@ import {
   CFormLabel,
   CModalFooter,
   CInputGroupText,
-  CFormSelect,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import {
@@ -66,7 +65,7 @@ const PencabutanReklame = () => {
   const [file, setFile] = useState()
   const [image, setImage] = useState('')
   const [ketetapan, setKetetapan] = useState([])
-  const statusFilter = 'Ijin Kadaluarsa'
+  const [statusFilter, setStatusFilter] = useState('Akan Dicabut')
   const [npwpdFilter, setNpwpdFilter] = useState('')
   const toaster = useRef()
   const today = new Date()
@@ -419,12 +418,21 @@ const PencabutanReklame = () => {
                     </CDropdown>
                   </CCol>
                   <CCol className="px-0">
-                    <CFormSelect aria-label="Default select example">
-                      <option>Status</option>
-                      <option value="1">One</option>
-                      <option value="2">Two</option>
-                      <option value="3">Three</option>
-                    </CFormSelect>
+                    <CDropdown variant="btn-group">
+                      <CButton variant="outline" color="dark" disabled>
+                        <CIcon icon={cilFilter} /> {statusFilter}
+                      </CButton>
+                      <CDropdownToggle variant="outline" color="secondary" />
+                      <CDropdownMenu>
+                        {['Ijin Kedaluarsa', 'Ijin Dicabut', 'Akan Dicabut', 'Sudah Dicabut'].map(
+                          (status) => (
+                            <CDropdownItem key={status} onClick={() => setStatusFilter(status)}>
+                              {status}
+                            </CDropdownItem>
+                          ),
+                        )}
+                      </CDropdownMenu>
+                    </CDropdown>
                   </CCol>
                 </CRow>
               </div>
